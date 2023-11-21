6ab365d7a6b663b797568f8dbeb1a9f6a5ea8617
Nyago56 <axelmfumulunanga@gmail.com>
2023-11-21 14:19:18 +0100

    V2.01

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import time
 i = 0
 print("Début")
 
-try:
-    while True:
+
+while True:
+    try:
         i = i + 1
         for o in range(i):
             print("CIAO", end='', flush=True)
         time.sleep(1)
         print(" ")
-except KeyboardInterrupt as Exc:
-    print(Exc)
+    except KeyboardInterrupt as Exc:
+        repr(Exc)
+        break
+
+    
     
 print("Fin")
 
