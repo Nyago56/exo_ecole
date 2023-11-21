3d4529b37e841fd335196e5dfaeeb7564207dace
Nyago56 <axelmfumulunanga@gmail.com>
2023-11-21 11:40:20 +0100

    V1.00

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import time
-i=0
-print("début")
-while True :
-	i=i+1
-	for o in range(i):
-		print("CIAO", end='', flush=True)
-	time.sleep(1)
-	print(" ")
-print("fin")
+
+i = 0
+print("Début")
+
+try:
+    while True:
+        i = i + 1
+        for o in range(i):
+            print("CIAO", end='', flush=True)
+        time.sleep(1)
+        print(" ")
+except KeyboardInterrupt:
+    print("\nInterruption du programme par l'utilisateur.")
+    
+print("Fin")
+
