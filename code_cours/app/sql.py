8a1e6db5035eefc4207c78ad5d53a304c68d044c
Nyago56 <axelmfumulunanga@gmail.com>
2023-11-22 15:55:45 +0100

    Prg structuré bdd

--- code_cours/app/sql.py
+++ code_cours/app/sql.py
@@ -4,6 +4,7 @@ tables = None
 cursor = None
 
 #=======================================================================================================================
+
 def init():
 
     global mydb
@@ -32,29 +33,40 @@ def init():
     # Exécutez une requête SQL pour récupérer des données de chaque table
     tables = ['adresse', 'camera', 'controle_fuite', 'equipement_industriel', 'langue',
             'mission', 'numero_mission', 'resident', 'scan_code_barre', 'site', 'utilisateur', 'utilise']
+    
 #-----------------------------------------------------------------------------------------------------------------------------
-def table():
+
+def gettable(table):
+        
+    print(f"\nContenu de la table '{table}':")
+
+    # Exécutez une requête SQL pour récupérer tous les éléments de la table
+    cursor.execute(f"SELECT * FROM {table}")
+
+    # Récupérez toutes les lignes résultantes
+    rows = cursor.fetchall()
+
+    # Affichez les résultats
+    for row in rows:
+        print(row)
+
+#-----------------------------------------------------------------------------------------------------------------------------
+
+def gettables():
     for table in tables:
-        print(f"\nContenu de la table '{table}':")
+        gettable(table)
 
-        # Exécutez une requête SQL pour récupérer tous les éléments de la table
-        cursor.execute(f"SELECT * FROM {table}")
-
-        # Récupérez toutes les lignes résultantes
-        rows = cursor.fetchall()
-
-        # Affichez les résultats
-        for row in rows:
-            print(row)
 #-----------------------------------------------------------------------------------------------------------------------------
+
 def close():
     # Fermez le curseur et la connexion
     cursor.close()
     mydb.close()
 
+
 #=======================================================================================================================
 if __name__=="__main__":
     init()
-    table()
-    table()
+    #gettables()
+    gettable('site')
     close()
