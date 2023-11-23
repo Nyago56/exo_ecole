0124f8d47b83f10d4b0fe55a5f40f0843d0ffca4
Nyago56 <axelmfumulunanga@gmail.com>
2023-11-23 14:41:04 +0100

    V1.00 fonctionnemnt 1 page

--- Myproject/app/ServWeb/routes.py
+++ Myproject/app/ServWeb/routes.py
@@ -5,12 +5,13 @@ from app.Bdd.bdd_recup import IF_bdd
 @app.route('/afficher_donnees')
 def afficher_donnees():
     bdd_instance = IF_bdd()
-    if bdd_instance.connect('172.19.0.1', 'votre_utilisateur', 'votre_mot_de_passe', 'votre_base_de_donnees'):
-        data_from_table = bdd_instance.get_data_from_table('votre_table')
-        return render_template('afficher_donnees.html', data=data_from_table)
+    if bdd_instance.connect('172.19.0.1', 'root', 'uimm', 'data_indus'):
+        data_from_table = bdd_instance.get_data_from_table('site')
+        return render_template('affiche_donnees.html', data=data_from_table)
     else:
         return "Erreur de connexion à la base de données."
 @app.route('/') #decorators
+
 @app.route('/index')
 def index():
     strResult = 'Hello Bruz'
