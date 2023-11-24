5248970fc72fca448a2566df5922caa7854d893c
Nyago56 <axelmfumulunanga@gmail.com>
2023-11-24 10:02:32 +0100

    V2.00 SITE WEB CONEXION + MISSION + ACCUEIL

--- Myproject/app/Bdd/bdd_recup.py
+++ Myproject/app/Bdd/bdd_recup.py
@@ -1,70 +1,65 @@
 import mysql.connector
-#=======================================================================================================================
-class IF_bdd:
-    "Classe object d'interface bdd sql"
-    def __init__(self):
 
+#=============================================================================================================
+
+class IF_bdd:
+
+#-------------------------------------------------------------------------------------------------------------
+
+    def __init__(self):
         self.mydb = None
-        self.tables = None
         self.cursor = None
 
-        print("IF_BDD : Connexion à la base de données")
-        
+#-------------------------------------------------------------------------------------------------------------
 
-         # Exécutez une requête SQL pour récupérer des données de chaque table
-        self.tables = ['adresse', 'camera', 'controle_fuite', 'equipement_industriel', 'langue',
-                'mission', 'numero_mission', 'resident', 'scan_code_barre', 'site', 'utilisateur', 'utilise']
-    
     def __del__(self):
-        if (self.mydb != None) :
-            #self.cursor.close()
+        if self.mydb is not None:
             self.mydb.close()
             print("IF_BDD : Fermeture bdd")
-    def connect(self,adrs,user,mdp,bdd_name):
+
+#-------------------------------------------------------------------------------------------------------------
+
+    def connect(self, adrs, user, mdp, bdd_name):
         try:
-            # Créez la connexion
             self.mydb = mysql.connector.connect(
                 host=adrs,
                 user=user,
                 password=mdp,
                 database=bdd_name
             )
-
         except Exception as Exc:
             print(repr(Exc))
-        if (self.mydb != None) :
-            # Créez un curseur
+
+        if self.mydb is not None:
             self.cursor = self.mydb.cursor()
             return True
-        else :
+        else:
             return False
         
-    def gettable(self,table):
-        if (self.mydb != None) :   
-            print(f"\nContenu de la table '{table}':")
-
-            # Exécutez une requête SQL pour récupérer tous les éléments de la table
-            self.cursor.execute(f"SELECT * FROM {table}")
-
-            # Récupérez toutes les lignes résultantes
-            rows = self.cursor.fetchall()
-
-            # Affichez les résultats
-            for row in rows:
-                print(row)
+#-------------------------------------------------------------------------------------------------------------
 
     def get_data_from_table(self, table):
         if self.mydb is not None:
+            # Execute a query to get the column names
+            self.cursor.execute(f"DESCRIBE {table}")
+            columns = [column[0] for column in self.cursor.fetchall()]
+
+            # Execute a query to get the data from the table
             self.cursor.execute(f"SELECT * FROM {table}")
             rows = self.cursor.fetchall()
-            return rows
+
+            return columns, rows
         else:
-            return []
-#=======================================================================================================================
+            return [], []
+        
+#-------------------------------------------------------------------------------------------------------------
+#=============================================================================================================
+
 if __name__ == "__main__":
     bdd_instance = IF_bdd()
     if bdd_instance.connect('172.19.0.1', 'root', 'uimm', 'data_indus'):
-        data_from_table = bdd_instance.get_data_from_table('site')
-        print(data_from_table)
+        columns, data_from_table = bdd_instance.get_data_from_table('site')
+        print("Columns:", columns)
+        print("Data:", data_from_table)
     else:
         print("Erreur de connexion à la base de données.")
--- Myproject/app/ServWeb/routes.py
+++ Myproject/app/ServWeb/routes.py
@@ -2,17 +2,25 @@ from app.ServWeb import app
 from flask import render_template
 from app.Bdd.bdd_recup import IF_bdd
 
+#--------------------------------------------------------------------------------------------------
+
 @app.route('/afficher_donnees')
 def afficher_donnees():
     bdd_instance = IF_bdd()
     if bdd_instance.connect('172.19.0.1', 'root', 'uimm', 'data_indus'):
-        data_from_table = bdd_instance.get_data_from_table('site')
-        return render_template('affiche_donnees.html', data=data_from_table)
+        columns, data_from_table = bdd_instance.get_data_from_table('mission')
+        return render_template('mission.html', columns=columns, data=data_from_table)
     else:
         return "Erreur de connexion à la base de données."
-@app.route('/') #decorators
 
-@app.route('/index')
+#--------------------------------------------------------------------------------------------------
+
+@app.route('/') #decorators
+def accueil():
+    return render_template('page_accueil.html')
+
+#--------------------------------------------------------------------------------------------------
+
+@app.route('/login')
 def index():
-    strResult = 'Hello Bruz'
-    return strResult
+     return render_template('login.html')
